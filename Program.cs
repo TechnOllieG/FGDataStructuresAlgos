e34f537ac9046e7cdc14a5c3c2683fd2405ef7d7
TechnOllieG <56596714+TechnOllieG@users.noreply.github.com>
2020-11-13 17:05:17 +0100

    Added BinaryTree (not done)

--- Program.cs
+++ Program.cs
@@ -4,13 +4,12 @@ namespace FGDataStructuresAlgos
 {
 	class Program
 	{
-		struct Test
-		{
-			public int hello;
-			public int yo;
-		}
-		
 		static void Main(string[] args)
+		{
+			LinkedListDemo();
+		}
+
+		static void LinkedListDemo()
 		{
 			int[] ints = new int[] {5, 64, 36, 75, 446, 35};
 			LinkedList<int> linkList = new LinkedList<int>(ints);
@@ -20,24 +19,7 @@ namespace FGDataStructuresAlgos
 				Console.WriteLine(node.Value);
 				node = node.Previous;
 			}
-
-			Test test = new Test();
-			test.hello = 5;
-			test.yo = 2;
 			
-			Test test2 = new Test();
-			test2.hello = 5;
-			test2.yo = 2;
-			
-			if (test.GetHashCode() == test2.GetHashCode())
-			{
-				Console.WriteLine("HashCode worked");
-			}
-
-			if (test.Equals(test2))
-			{
-				Console.WriteLine("Equals worked");
-			}
 			Console.WriteLine(linkList.Contains(34));
 		}
 	}
